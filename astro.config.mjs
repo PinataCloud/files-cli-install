0733beaa791584d7932f1d177b304d52272b32a7
Steve <stevedsimkins@gmail.com>
2024-10-10 12:00:16 -0400

    updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-	redirects: {
-		"/": {
-			status: 302,
-			destination: "https://docs.pinata.cloud/tools/files-cli",
-		},
-	},
-});
+export default defineConfig({});
