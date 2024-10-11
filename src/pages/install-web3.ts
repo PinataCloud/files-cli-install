4fb23ac97379c6fffe1227c6118060ba802ceacc
Steve <stevedsimkins@gmail.com>
2024-10-11 11:13:40 -0400

    added install route for web3

---
+++ src/pages/install-web3.ts
@@ -0,0 +1,15 @@
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async () => {
+	const data = await fetch(
+		"https://files-cli-install.vercel.app/script-web3.sh",
+	);
+	const file = await data.blob();
+	const headers = new Headers();
+	headers.set("Content-Type", "application/octet-stream");
+	headers.set("Content-Disposition", 'attachment; filename="install.sh"');
+	return new Response(file, {
+		status: 200,
+		headers: headers,
+	});
+};
