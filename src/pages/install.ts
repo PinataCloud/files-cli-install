75fcc1bf105ec5069fa5f1322b1a472ec07f1db4
Steve <stevedsimkins@gmail.com>
2025-03-06 14:08:54 -0500

    chore: Updated install route

--- src/pages/install.ts
+++ src/pages/install.ts
@@ -1,13 +1,13 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async () => {
-	const data = await fetch("https://files-cli-install.vercel.app/script.sh");
-	const file = await data.blob();
-	const headers = new Headers();
-	headers.set("Content-Type", "application/octet-stream");
-	headers.set("Content-Disposition", 'attachment; filename="install.sh"');
-	return new Response(file, {
-		status: 200,
-		headers: headers,
-	});
+  const data = await fetch("https://cli.pinata.cloud/script.sh");
+  const file = await data.blob();
+  const headers = new Headers();
+  headers.set("Content-Type", "application/octet-stream");
+  headers.set("Content-Disposition", 'attachment; filename="install.sh"');
+  return new Response(file, {
+    status: 200,
+    headers: headers,
+  });
 };
