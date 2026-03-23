9ed8a8369be1a5e21787c30c0e0d15998b0fde24
iammatthias <matthias@pinata.cloud>
2026-03-23 15:39:30 -0700

    Bump packages and correct docs link

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
-
+import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind({
-        applyBaseStyles: false,
-		}), react()],
+    integrations: [react()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
